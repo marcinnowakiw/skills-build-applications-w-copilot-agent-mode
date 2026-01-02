a520cedb309fe544ae13800686eb959559b59ddf
marcinnowakiw <m.b.nowak@accenture.com>
2026-01-02 15:34:59 +0000

    feat: add ObjectIdField to serializers for MongoDB compatibility

--- octofit-tracker/backend/octofit_tracker/serializers.py
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -1,27 +1,45 @@
 from rest_framework import serializers
 from .models import User, Team, Activity, Workout, Leaderboard
 
+
+from bson import ObjectId
+
+class ObjectIdField(serializers.Field):
+    def to_representation(self, value):
+        return str(value) if isinstance(value, ObjectId) else value
+    def to_internal_value(self, data):
+        return ObjectId(str(data))
+
 class TeamSerializer(serializers.ModelSerializer):
+    id = ObjectIdField(read_only=True)
     class Meta:
         model = Team
         fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
+    id = ObjectIdField(read_only=True)
+    team = ObjectIdField(required=False, allow_null=True)
     class Meta:
         model = User
         fields = '__all__'
 
 class ActivitySerializer(serializers.ModelSerializer):
+    id = ObjectIdField(read_only=True)
+    user = ObjectIdField()
     class Meta:
         model = Activity
         fields = '__all__'
 
 class WorkoutSerializer(serializers.ModelSerializer):
+    id = ObjectIdField(read_only=True)
+    suggested_for = serializers.ListField(child=ObjectIdField(), required=False)
     class Meta:
         model = Workout
         fields = '__all__'
 
 class LeaderboardSerializer(serializers.ModelSerializer):
+    id = ObjectIdField(read_only=True)
+    team = ObjectIdField()
     class Meta:
         model = Leaderboard
         fields = '__all__'
